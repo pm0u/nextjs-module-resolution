818b6d791998bf9d5a8f20561aef5507ba7beb06
Paul Mourer <pmourer@joinhandshake.com>
2022-11-16 10:16:07 -0700

    Adding some helpful messaging

--- ignore.sh
+++ ignore.sh
@@ -1,8 +1,10 @@
 #! /bin/bash
 
 if [ "$CACHED_COMMIT_REF" != "$COMMIT_REF" ]; then
+  echo "New Commit - Checking for changes..."
   git diff --quiet $CACHED_COMMIT_REF $COMMIT_REF
   exit
 fi
 
+echo "Looks like we already deployed this commit, proceeding with forced re-deploy..."
 exit 1
